af85a2d7b6f0e979e3f68d58a0470e7195f73fc6
andy <13021013096@163.com>
2022-10-02 00:20:41 +0800

    初始化项目

---
+++ Server/Server/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  Server
+//
+//  Created by Andy on 2022/10/2.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
